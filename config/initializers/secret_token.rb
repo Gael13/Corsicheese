0defd39b9f33676eba292f965bfd0597e8943289
vagrant <vagrant@precise64.(none)>
2014-03-18 17:37:19 +0000

    Depot products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Corsicheese::Application.config.secret_key_base = 'ee255c3f8a16f892e0a99ca6cd12b51a8baee08410808a19738fd29262fd160dc5845db533e63310a2424f036ae539f3911248641cd0e1da371eb8dafca6da1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Corsicheese::Application.config.session_store :cookie_store, key: '_Corsicheese_session'
